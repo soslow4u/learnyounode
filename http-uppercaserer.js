38401b2dfd0a1e84164384ac4803df238bc5ffbb
slowest <soslow4u@gmail.com>
2020-08-03 21:27:36 +0900

    Revert "step 11"
    This reverts commit abcc3883b34a5b4fd325ae115fc92ebb98fd6d9a.

--- http-uppercaserer.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const map =  require('through2-map');
-
-http.createServer(
-    (request, response) =>{
-        if(request.method === 'POST')
-            request.pipe(map((chunk)=>chunk.toString().toUpperCase())).pipe(response);
-    }
-).listen(Number(process.argv[2]));
